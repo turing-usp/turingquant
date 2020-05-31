96d8cf417f4219bc8689dc3ff14b4d94d48af259
lucas-leme <lucaslssantos99@gmail.com>
2020-05-31 17:36:23 -0300

    Add alpha, beta and sharpe

---
+++ metrics.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def sharpe_ratio (returns, risk_free = 0):
+
+    expected_returns = returns.mean()
+
+    risk = returns.std()
+
+    return (expected_returns - risk_free) / risk
+
+def beta (returns, benchmark):
+
+    concat = np.matrix([returns, benchmark])
+
+    cov = np.cov(concat)[0][1]
+
+    benchmark_vol = np.std(benchmark)
+
+    return cov / benchmark_vol
+
+def alpha (end_price, dps, start_price):
+
+    return (end_price + dps - start_price) / start_price
+
+def test_metrics ():
+
+        returns = np.random.uniform(-1, 1, 50)
+        market = np.random.uniform(-1, 1, 50)
+
+        start
+
+        print("Sharpe: ", sharpe_ratio(returns))
+        print("Beta: ", beta(returns, market))
