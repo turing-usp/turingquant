30a5dc151e3e17da4d0299878b845d156256de96
Noel <noel.eliezer@usp.br>
2020-09-01 00:19:32 -0300

    bump version (#11)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="turingquant",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=["pandas", "pandas_datareader", "numpy", "matplotlib", "alpha_vantage", "bs4"],
 
