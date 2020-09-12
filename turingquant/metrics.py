4ca6186105e16cd0f223603f6537a04c3f0808cd
anor4k <noel.eliezer@usp.br>
2020-09-12 17:21:26 -0300

    implementa rolling_beta

--- turingquant/metrics.py
+++ turingquant/metrics.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+import plotly.express as px
 
 
 def sharpe_ratio(returns, risk_free=0):
@@ -17,7 +19,7 @@ def sharpe_ratio(returns, risk_free=0):
     return(expected_returns - risk_free) / risk
 
 
-def beta(returns, benchmark):
+def beta(returns, benchmark, rolling_window=None, plot=False):
     """
     Essa função, a partir do fornecimento dos retornos do ativo e do benchmark, calcula o beta do ativo.
 
@@ -25,7 +27,6 @@ def beta(returns, benchmark):
 
     benchmark [pd.series]: série com o retorno do benchmark
     """
-
     concat = np.matrix([returns, benchmark])
     cov = np.cov(concat)[0][1]
     benchmark_vol = np.std(benchmark)
@@ -45,6 +46,36 @@ def alpha(end_price, dps, start_price):
     """
 
     return(end_price + dps - start_price) / start_price
+
+
+def rolling_beta(returns, benchmark, window, plot=True):
+    """
+    Plota o beta móvel para um ativo e um benchmark de referência, na forma de séries de retornos.
+
+    Parâmetros:
+        returns (array): série de retornos para o qual o beta será calculado.
+        benchmark (array): série de retornos para usar de referência no cálculo do beta.
+        window (int): janela móvel para calcular o beta ao longo do tempo.
+        plot (bool): se `True`, plota um gráfico de linha com o beta ao longo do tempo.
+
+    Retorna:
+        rolling_beta (pd.Series): uma série com os valores do Beta para os últimos `window` dias.
+            A série não possui os `window` primeiros dias.
+
+    """
+    returns = pd.DataFrame(returns)
+    benchmark = pd.DataFrame(benchmark)
+    merged = returns.merge(benchmark, left_index=True, right_index=True)
+    rolling_beta = merged.iloc[:, 0].rolling(window).cov(merged.iloc[:, 1])/merged.iloc[:, 1].rolling(window).var()
+    rolling_beta = rolling_beta[window:]
+    rolling_beta.name = 'beta'
+    if plot:
+        fig = px.line(rolling_beta, title="Beta móvel")
+        fig.update_layout(showlegend=False)
+        fig.update_xaxes(title_text='Tempo')
+        fig.update_yaxes(title_text='Beta móvel: ' + str(window) + ' dias')
+        fig.show()
+    return rolling_beta
 
 
 def test_metrics():
