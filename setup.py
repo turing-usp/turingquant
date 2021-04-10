15a9b62e12f802fbcb103ee125dbbd2f1e63b4fa
lucas-leme <lucaslssantos99@gmail.com>
2021-04-10 10:20:11 -0300

    Update setup.py version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="turingquant",
-    version="0.1.9",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=["pandas", "pandas_datareader", "numpy", "matplotlib", "alpha_vantage", "bs4", "plotly", "yfinance"],
 
