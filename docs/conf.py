c5ffa6b5051aef7d965bca54b278dd4de1234e6e
lucas-leme <lucaslssantos99@gmail.com>
2021-05-31 18:31:37 -0300

    Bump version 0.2.2

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = 'Grupo Turing'
 master_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.1'
+release = '0.2.2'
 
 
 # -- General configuration ---------------------------------------------------
